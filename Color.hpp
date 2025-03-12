aded9f65d96d3ce828fd3d42ade0c467c10d3b58
Arianna Ferrari <arianna.03ottobre@gmail.com>
2025-03-12 12:30:43 +0100

    Add sum of two colors

--- Color.hpp
+++ Color.hpp
@@ -4,5 +4,9 @@ public:
 
     Color() : r(0.0f), g(0.0f), b(0.0f) {}
     Color(float r, float g, float b) : r(r), g(g), b(b) {}
+
+    Color operator+(const Color& other) const {
+        return Color(r + other.r, g + other.g, b + other.b);
+    }
 };
 
