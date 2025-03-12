ed074fb98eefd29e70e87d684c16185f446e1498
enrico <enrico.carissimi1@studenti.unimi.it>
2025-03-12 12:22:50 +0100

    Fixed constructor

--- Color.hpp
+++ Color.hpp
@@ -1,10 +1,8 @@
 class Color {
-
-    public:
+public:
+    float r, g, b;
 
     Color() : r(0.0f), g(0.0f), b(0.0f) {}
-    Color(r, g, b) : r(r), g(g), b(b) {}
-
-    float r, g, b;
-}
+    Color(float r, float g, float b) : r(r), g(g), b(b) {}
+};
 
