8ee2d8878e7a66d98c9b3a0975e52c652f629216
Arianna Ferrari <arianna.03ottobre@gmail.com>
2025-03-13 21:00:08 +0100

    Added multiplication by a scalar

--- Color.hpp
+++ Color.hpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #ifndef __Color__
 #define __Color__
 
@@ -15,6 +16,17 @@ public:
     Color operator+(const Color& other) const {
         return Color(r + other.r, g + other.g, b + other.b);
     }
+
+    Color operator*(float scalar) const {
+        return Color(r * scalar, g * scalar, b * scalar);
+    }
+
+    bool isClose(const Color& other, float epsilon = 1e-5f) const {
+        return (std::fabs(r - other.r) < epsilon) &&
+               (std::fabs(g - other.g) < epsilon) &&
+               (std::fabs(b - other.b) < epsilon);
+    }
+    
 };
 
 #endif
