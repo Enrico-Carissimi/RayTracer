6dbe3d4a78bc35d49b15cb66e662b38aca4b792d
enrico <enrico.carissimi1@studenti.unimi.it>
2025-04-08 10:07:33 +0200

    add tests for saving images

--- test/testPFMreader.cpp
+++ test/testPFMreader.cpp
@@ -4,7 +4,7 @@
 using std::cout, std::endl;
 
 // This is the content of "reference_le.pfm" (little-endian file)
-unsigned char LE_REFERENCE_BYTES[] = {
+const unsigned char LE_REFERENCE_BYTES[] = {
     0x50, 0x46, 0x0a, 0x33, 0x20, 0x32, 0x0a, 0x2d, 0x31, 0x2e, 0x30, 0x0a,
     0x00, 0x00, 0xc8, 0x42, 0x00, 0x00, 0x48, 0x43, 0x00, 0x00, 0x96, 0x43,
     0x00, 0x00, 0xc8, 0x43, 0x00, 0x00, 0xfa, 0x43, 0x00, 0x00, 0x16, 0x44,
@@ -15,7 +15,7 @@ unsigned char LE_REFERENCE_BYTES[] = {
 };
 
 // This is the content of "reference_be.pfm" (big-endian file)
-unsigned char BE_REFERENCE_BYTES[] = {
+const unsigned char BE_REFERENCE_BYTES[] = {
     0x50, 0x46, 0x0a, 0x33, 0x20, 0x32, 0x0a, 0x31, 0x2e, 0x30, 0x0a,
     0x42, 0xc8, 0x00, 0x00, 0x43, 0x48, 0x00, 0x00, 0x43, 0x96, 0x00, 0x00,
     0x43, 0xc8, 0x00, 0x00, 0x43, 0xfa, 0x00, 0x00, 0x44, 0x16, 0x00, 0x00,
@@ -25,10 +25,24 @@ unsigned char BE_REFERENCE_BYTES[] = {
     0x42, 0x8c, 0x00, 0x00, 0x42, 0xa0, 0x00, 0x00, 0x42, 0xb4, 0x00, 0x00
 };
 
-void testRead(unsigned char* bytes, int length) {
+const unsigned char TEST_WRITE_BYTES[] = {
+    0x50, 0x46, 0x0a, 0x33, 0x20, 0x32, 0x0a, 0x2d, 0x31, 0x2e, 0x30, 0x0a,
+    0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00, // yellow
+    0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x3f, // magenta
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // black
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00, // green
+    0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x80, 0x3f, // white
+    0x00, 0x00, 0x80, 0x3f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  // red
+};
+
+auto streamFromArray(const unsigned char* bytes, int length) {
     std::stringstream ss;
     for (int i = 0; i < length; i++) ss << bytes[i]; // make stream from array of bytes
-    std::istringstream stream(ss.str());
+    return std::istringstream(ss.str());
+}
+
+void testRead(const unsigned char* bytes, int length) {
+    auto stream = streamFromArray(bytes, length);
     HDRImage image(stream);
 
     assert(image._width == 3);
@@ -42,7 +56,7 @@ void testRead(unsigned char* bytes, int length) {
     assert(image.getPixel(2, 1).isClose(Color(7.0e2, 8.0e2, 9.0e2)));
 }
 
-void testReadFile(std::string name, int length) {
+void testReadFile(std::string name) {
     HDRImage image(name);
 
     assert(image._width == 3);
@@ -56,56 +70,92 @@ void testReadFile(std::string name, int length) {
     assert(image.getPixel(2, 1).isClose(Color(7.0e2, 8.0e2, 9.0e2)));
 }
 
+void testWritePMF() {
+    auto stream = streamFromArray(LE_REFERENCE_BYTES, 84);
+    HDRImage image(stream);
+    image.save("test/test.pfm");
+    testReadFile("test/test.pfm");
+}
+
 bool isClose(float a, float b) {
     return std::fabs(a - b) < 1e-6;
 }
 
-/* def test_pfm_read_wrong(self):
-    buf = BytesIO(b"PF\n3 2\n-1.0\nstop")
-    with pytest.raises(InvalidPfmFileFormat):
-        _ = read_pfm_image(buf) */
+// tests if an exception is thrown by a function with the specified parameter
+// if the function accepts more parameters, or if it is a constructor, use a lambda: 
+// testException(parameter, [](Parameter p) -> {return function(p, [...]);});
+template<typename Parameter, typename Function>
+void testException(Parameter& parameter, Function function) {
+    bool exceptionThrown = false;
+    try{function(parameter);}
+    catch(std::exception& e){exceptionThrown = true;}
+    assert(exceptionThrown);
+}
+
+
 
 int main(){
+    bool exceptionThrown = false; // used to test exception, reset to false after each test
+
     // test readLine
     std::istringstream iss("hiii\nthis is a test!");
     assert(readLine(iss) == "hiii");
     assert(readLine(iss) == "this is a test!");
-    //std::string buffer = readLine(iss); // this throws an error
+    testException(iss, readLine);
     cout << "readLine works" << endl;
 
     // test parseImageSize
     assert(parseImageSize("2 5") == std::make_pair(2, 5));
-    //auto size1 = parseImageSize("-2 5"); // negative size
-    //auto size2 = parseImageSize("2 0"); // zero size
-    //auto size3 = parseImageSize("2 a"); // not int
-    //auto size4 = parseImageSize("2"); // string too short
-    //auto size5 = parseImageSize("2 2 a"); // string too long
+    testException("-2 5", parseImageSize); // negative size
+    testException("2 0", parseImageSize); // zero size
+    testException("2 a", parseImageSize); // not int
+    testException("2", parseImageSize); // string too short
+    testException("2 5 3", parseImageSize); // string too long
     cout << "parseImageSize works" << endl;
 
     // test parseEndianness
     assert(parseEndianness("-1.0") == Endianness::LITTLE);
     assert(parseEndianness("1000.0") == Endianness::BIG);
-    //auto endianness1 = parseEndianness("endianness"); // can't convert to float
-    //auto endianness2 = parseEndianness("0.0"); // is zero
+    testException("e", parseEndianness); // can't convert to float
+    testException("0.0", parseEndianness); // is zero
     cout << "parseEndianness works" << endl;
 
     // test readFloat
-    unsigned char toRead[] = {0x00, 0x00, 0xc8, 0x42, 0x43, 0x48, 0x00, 0x00}; // {100 (le), 200 (be)}
-    std::stringstream ss;
-    for (int i = 0; i < 8; i++) ss << toRead[i]; // make stream from array of bytes
-    std::istringstream stream(ss.str());
-    
+    unsigned char toRead[] = {0x00, 0x00, 0xc8, 0x42, 0x43, 0x48, 0x00, 0x00, 0x00}; // {100 (le), 200 (be), bonus byte}
+    auto stream = streamFromArray(toRead, 9);
     assert(isClose(readFloat(stream, Endianness::LITTLE), 100));
     assert(isClose(readFloat(stream, Endianness::BIG), 200));
+    testException(stream, [](std::istringstream& ss) -> auto {return readFloat(ss, Endianness::BIG);}); // only 1 byte left
     cout << "readFloat works" << endl;
 
+    // test reading images
     testRead(LE_REFERENCE_BYTES, 84);
     testRead(BE_REFERENCE_BYTES, 83);
+    std::istringstream ss("PF\n3 2\n-1.0\nstop");
+    testException(ss, [](std::istringstream& s) -> auto {return HDRImage(s);});
     cout << "readPFM works" << endl;
 
-    testReadFile("test/reference_le.pfm", 84);
-    testReadFile("test/reference_be.pfm", 83);
+    testReadFile("test/reference_le.pfm");
+    testReadFile("test/reference_be.pfm");
+    HDRImage image("test/memorial.pfm");
+    testException("test/iDoNotExist.psd", testReadFile);
     cout << "readPFM from file works" << endl;
+
+    // test writePFM
+    testWritePMF();
+    cout << "writePFM works" << endl;
+
+    // test writing LDR formats
+    stream = streamFromArray(TEST_WRITE_BYTES, 84);
+    image = HDRImage(stream);
+    image.normalizeImage(1., image.averageLuminosity());
+    image.clampImage();
+    image.save("test/test.png");
+    image.save("test/test.jpeg");
+    testException("test/test.gif", [&image](std::string s) -> auto {return image.save(s);});
+    cout << "there should be \"test.*\" files containing 6 pixels: g, w, r / y, m, k" << endl;
+
+
 
     /* // test readPFM
     unsigned char small[] = {0x50, 0x46, 0x0a, 0x31, 0x20, 0x31, 0x0a, 0x2d, 0x31, 0x2e, 0x30, 0x0a,
