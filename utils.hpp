6cb2b1c4a6b6863fa6865892b250a5a6c7b49162
Arif031098 <arianna.03ottobre@gmail.com>
2025-04-28 10:38:04 +0200

    copy utils.hpp in geometry

---
+++ utils.hpp
@@ -0,0 +1,25 @@
+#ifndef __utils__
+#define __utils__
+
+#include <cmath>
+#include <string>
+
+inline float degToRad(float degrees){return degrees * M_PI / 180.;}
+
+bool areClose(float a, float b, float epsilon = 1e-5f) {
+    return std::fabs(a - b) < epsilon;
+}
+
+bool areCloseMatrix(const float A[16], const float B[16], float epsilon = 1e-5f) {
+    for (int i = 0; i < 16; i++) {
+        if (!areClose(A[i], B[i])) return false;
+    }
+    return true;
+}
+
+template <typename T1>
+bool areClose(const T1& v, const T1& u, float epsilon = 1e-5f) {
+    return (areClose(v.x, u.x, epsilon) && areClose(v.y, u.y, epsilon) && areClose(v.z, u.z, epsilon));
+}
+
+#endif
