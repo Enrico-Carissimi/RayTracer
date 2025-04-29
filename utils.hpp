5f2f6ba5d5a9380d21b32469b1a71a7f09108932
Arianna Ferrari <arianna.03ottobre@gmail.com>
2025-04-29 17:10:19 +0200

    commit.Modify areCloseMatrix

--- utils.hpp
+++ utils.hpp
@@ -10,12 +10,13 @@ bool areClose(float a, float b, float epsilon = 1e-5f) {
     return std::fabs(a - b) < epsilon;
 }
 
-bool areCloseMatrix(const float A[16], const float B[16], float epsilon = 1e-5f) {
+bool areCloseMatrix(const float A[16], const float B[16], float epsilon = 1e-4f) {
     for (int i = 0; i < 16; i++) {
-        if (!areClose(A[i], B[i])) return false;
+        if (!areClose(A[i], B[i], epsilon)) return false;
     }
     return true;
 }
+
 
 template <typename T1>
 bool areClose(const T1& v, const T1& u, float epsilon = 1e-5f) {
