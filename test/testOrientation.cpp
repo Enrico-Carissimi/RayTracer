04c8f5012bbd90ade4f769ca1f3fabb4638ca783
enrico <enrico.carissimi1@studenti.unimi.it>
2025-05-07 10:40:06 +0200

    temporary: add test that should fail to check why the workflow doesnt't

---
+++ test/testOrientation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "../Camera.hpp"
+
+const float aspectRatio = 2.;
+const float distance = 1.;
+
+Camera setup() { return Camera("perspective", aspectRatio, 4); }
+
+void testOrientation() {
+    Camera camera = setup();
+    
+    Ray topLeftRay = camera.castRay(0, 0, 0., 0.);
+    assert(Point3(0., 2., 1.).isClose(topLeftRay.at(1.)));
+
+    Ray bottomRightRay = camera.castRay(3, 1, 1., 1.);
+    assert(Point3(0., -2., -1.).isClose(bottomRightRay.at(1.)));
+
+    std::cout << "the image is oriented correctly" << std::endl;
+}
+
+int main() {
+    testOrientation();
+
+    return 0;
+}
