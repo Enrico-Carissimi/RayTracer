2eecca6caa3fdf7221bb14dbe81bd35c21d60227
enrico <enrico.carissimi1@studenti.unimi.it>
2025-05-07 10:54:20 +0200

    add sleep to temporary test to see if it really runs

--- test/testOrientation.cpp
+++ test/testOrientation.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "../Camera.hpp"
+#include <chrono>
+#include <thread>
 
 const float aspectRatio = 2.;
 const float distance = 1.;
@@ -7,6 +9,8 @@ const float distance = 1.;
 Camera setup() { return Camera("perspective", aspectRatio, 4); }
 
 void testOrientation() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    
     Camera camera = setup();
     
     Ray topLeftRay = camera.castRay(0, 0, 0., 0.);
