098caf23e1bedd4b1f709a8b23051d770129bb07
enrico <enrico.carissimi1@studenti.unimi.it>
2025-06-08 11:26:51 +0200

    move testPathTracer to testRenderers

--- test/testRenderers.cpp
+++ test/testRenderers.cpp
@@ -43,7 +43,7 @@ void testFlatRenderer() {
     sassert(camera.image.getPixel(2, 0).isClose(BLACK));
 
     sassert(camera.image.getPixel(0, 1).isClose(BLACK));
-    sassert(camera.image.getPixel(1, 1).isClose(color * (1. / PI))); // account for the factor 1/PI in the reflectance
+    sassert(camera.image.getPixel(1, 1).isClose(color)); // account for the factor 1/PI in the reflectance
     sassert(camera.image.getPixel(2, 1).isClose(BLACK));
 
     sassert(camera.image.getPixel(0, 2).isClose(BLACK));
@@ -53,11 +53,48 @@ void testFlatRenderer() {
     std::cout << "flat renderer works" << std::endl;
 }
 
+void testPathTracer() { // furnace test
+    PCG pcg;
+
+    for (int i = 0; i < 5; ++i) {
+        World world;
+
+        float emittedRadiance = pcg.random();
+        float reflectance = pcg.random() * 0.9;
+
+        Color white(1.0f, 1.0f, 1.0f);
+        auto emitted = std::make_shared<UniformTexture>(white * emittedRadiance);
+        auto texture = std::make_shared<UniformTexture>(white * reflectance);
+        auto material = std::make_shared<DiffuseMaterial>(texture, emitted);
+
+        world.addShape(std::make_shared<Sphere>(material));
+
+        Ray ray(Point3(0, 0, 0), Vec3(1, 0, 0));
+
+        Color result = Renderers::PathTracer(ray, world, pcg, 1, 100, 101);
+
+        float expected = emittedRadiance / (1.0f - reflectance);
+
+        std::cout << "Test " << i + 1 << "\n";
+        std::cout << "  emitted = " << emittedRadiance << ", reflectance = " << reflectance << "\n";
+        std::cout << "  expected = " << expected << ", got = (" << result.r << ", " << result.g << ", " << result.b << ")\n";
+
+        sassert(areClose(result.r, expected, 1e-3));
+        sassert(areClose(result.g, expected, 1e-3));
+        sassert(areClose(result.b, expected, 1e-3));
+    }
+
+    std::cout << "furnace tests works" << std::endl;
+}
+
 
 
 int main() {
     testOnOffRenderer();
     testFlatRenderer();
+    testPathTracer();
+
+    std::cout << "All tests passed!\n";
 
     return 0;
 }
