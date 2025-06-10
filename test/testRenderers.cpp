696ff70810c1a1926d4150acc3d5140297410587
Arianna Ferrari <arianna.03ottobre@gmail.com>
2025-06-10 10:42:53 +0200

    Add test for PointLight tracer

--- test/testRenderers.cpp
+++ test/testRenderers.cpp
@@ -2,8 +2,12 @@
 #include "../renderers.hpp"
 #include "../utils.hpp"
 
-const Color BLACK(0., 0., 0.);
-const Color WHITE(1., 1., 1.);
+const Color BLACK(0.0f, 0.0f, 0.0f);
+const Color WHITE(1.0f, 1.0f, 1.0f);
+const Color RED(1.0f, 0.0f, 0.0f);
+const Color GREEN(0.0f, 1.0f, 0.0f);
+const Color BLUE(0.0f, 0.0f, 1.0f);
+const Color YELLOW(1.0f, 1.0f, 0.0f);
 
 void testOnOffRenderer() {
     auto material = std::make_shared<DiffuseMaterial>(std::make_shared<UniformTexture>(WHITE));
@@ -53,11 +57,46 @@ void testFlatRenderer() {
     std::cout << "flat renderer works" << std::endl;
 }
 
+void testPointLight() {
+    World world;
+
+    auto sphereMaterial = std::make_shared<DiffuseMaterial>(
+        std::make_shared<UniformTexture>(RED),
+        std::make_shared<UniformTexture>(BLUE)
+    );
+
+    world.addShape(std::make_shared<Sphere>(sphereMaterial));
+
+    Point3 lightPos(-2.0f, -1.0f, 0.0f);
+
+    world.addLight(PointLight{
+        lightPos,
+        YELLOW
+    });
+
+    Ray ray(Point3(-2.0f, 0.0f, 0.0f), Vec3(1.0f, 0.0f, 0.0f));
+
+    Color color = Renderers::PointLight(ray, world, GREEN);
+
+    float cosTheta = std::cos(PI / 4.0f);  // θ = 45°
+    Color brdf = RED * (1.0f / PI);
+    Color expected = GREEN + (BLUE + brdf) * YELLOW * cosTheta;
+
+    if (!areClose(expected.r, color.r) || !areClose(expected.g, color.g) || !areClose(expected.b, color.b)) {
+        std::cerr << "Test failed!\n";
+        std::cerr << "Expected: (" << expected.r << ", " << expected.g << ", " << expected.b << ")\n";
+        std::cerr << "Got:      (" << color.r << ", " << color.g << ", " << color.b << ")\n";
+    } else {
+        std::cout << "Test passed!\n";
+    }
+}
+
 
 
 int main() {
     testOnOffRenderer();
     testFlatRenderer();
+    testPointLight();
 
     return 0;
 }
