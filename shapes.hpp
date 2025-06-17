bd0e9f7cb463d4a4b8d85d43c9cd5b62f526a267
enrico <enrico.carissimi1@studenti.unimi.it>
2025-06-17 12:58:58 +0200

    add quick hit method for spheres and use delta/4 in sphere hit

--- shapes.hpp
+++ shapes.hpp
@@ -45,19 +45,19 @@ public:
     
     bool isHit(const Ray& r, HitRecord& rec) const override {
             
-        Ray invRay = r.transform(transformation.inverse());  
+        Ray invRay = r.transform(transformation.inverse());
     
         Vec3 originVec = invRay.origin.toVec();
         float a = invRay.direction.norm2();
-        float b = 2.0f * dot(originVec, invRay.direction);  
+        float b = dot(originVec, invRay.direction); // actually is b/2
         float c = originVec.norm2() - 1.0f;
 
-        float delta = b * b - 4.0f * a * c;
-        if (delta <= 0.0f) return false;  
+        float delta = b * b - a * c; // delta/4
+        if (delta <= 0.0f) return false;
 
         float sqrtDelta = std::sqrt(delta);
-        float t1 = (-b - sqrtDelta) / (2.0f * a);
-        float t2 = (-b + sqrtDelta) / (2.0f * a);
+        float t1 = (-b - sqrtDelta) / a;
+        float t2 = (-b + sqrtDelta) / a;
     
         float t = -1.0f;
         if (t1 > invRay.tmin && t1 < invRay.tmax) {
@@ -65,19 +65,36 @@ public:
         } else if (t2 > invRay.tmin && t2 < invRay.tmax) {
             t = t2;
         } else {
-            return false;  
+            return false;
         }
     
         Point3 localHit = invRay.at(t);
     
         rec.t = t;
         rec.ray = r;
-        rec.worldPoint = transformation * localHit;  
-        rec.normal = transformation * sphereNormal(localHit, invRay.direction);  
+        rec.worldPoint = transformation * localHit;
+        rec.normal = transformation * sphereNormal(localHit, invRay.direction);
         rec.surfacePoint = sphereUV(localHit);
         rec.material = _material;
     
         return true;
+    }
+
+    bool quickIsHit(const Ray& ray) const override {
+        Ray invRay = ray.transform(transformation.inverse());
+        Vec3 origin = invRay.origin.toVec();
+        float a = invRay.direction.norm2();
+        float b = dot(origin, invRay.direction);
+        float c = origin.norm2() - 1.0;
+
+        float delta = b * b - a * c;
+        if (delta <= 0.0f) return false;
+
+        float sqrtDelta = std::sqrt(delta);
+        float tmin = (-b - sqrtDelta) / a;
+        float tmax = (-b + sqrtDelta) / a;
+
+        return (invRay.tmin < tmin && invRay.tmax > tmin) || (invRay.tmin < tmax && invRay.tmax > tmax);
     }
 };
 
