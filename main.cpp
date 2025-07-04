48fe6ccaf4def4cc950679e191df0f7b4960d9d1
enrico <enrico.carissimi1@studenti.unimi.it>
2025-07-04 23:05:01 +0200

    add render command to render image from scene file
    this was done by adapting the demo command adding new CLI options

--- main.cpp
+++ main.cpp
@@ -1,65 +1,125 @@
 #include "Camera.hpp"
 #include "World.hpp"
 #include "renderers.hpp"
+#include "Scenefile.hpp"
 #include "lib/CLI11.hpp"
 
-using std::shared_ptr, std::make_shared;
 
-// Demo command to generate an example image
-void demo(std::string output, int nRays, int maxDepth, int russianRouletteLimit, int AAsamples, float angle) {
-    Camera camera("perspective", 4. / 3., 640, 1., rotation(angle, Axis::Z) * translation(-1., 0., 0.));
-    World world;
 
-    auto material = make_shared<DiffuseMaterial>(make_shared<UniformTexture>(Color(1., 0.5, 0.3)));
-    auto skyMaterial = make_shared<DiffuseMaterial>(make_shared<UniformTexture>(Color()), make_shared<UniformTexture>(Color(1., 1., 1.)));
-    auto reflectMaterial = make_shared<SpecularMaterial>(make_shared<UniformTexture>(Color(0.1, 0.9, 0.4)), make_shared<UniformTexture>(Color()), 1.);
-    auto floorMaterial = make_shared<DiffuseMaterial>(make_shared<CheckeredTexture>(Color(0., 1., 1.), Color(1., 0., 1.), 4));
+/**
+ * @brief validate a string in the format "name:value", where value is convertible to float
+ * 
+ * @param s string to validate
+ * @param floatVariables container for name-value pairs
+ */
+void validateFloatVariable(std::string& s, std::unordered_map<std::string, float>& floatVariables) {
+    std::string key;
+    float value;
 
-    world.addShape(make_shared<Sphere>(material, translation(0., 0., -0.2) * scaling(0.8)));
-    world.addShape(make_shared<Sphere>(skyMaterial, translation(0., 0., 0.) * scaling(1000.)));
-    world.addShape(make_shared<Sphere>(reflectMaterial, translation(1., 2., -0.9)));
-    world.addShape(make_shared<Plane>(floorMaterial, translation(0., 0., -1.)));
+    size_t position = s.find(':');
+    if (position == std::string::npos)
+        throw std::invalid_argument("ERROR: \"" + s + "\" does not define a float variable\n"
+                                    "the correct syntax is --float=name:value");
 
-    
+    key = s.substr(0, position);
 
-    camera.render(Renderers::PathTracer, AAsamples, world, camera.pcg, nRays, maxDepth, russianRouletteLimit);
-    camera.image.save("demo.pfm");
-    camera.image.normalize(1.);
-    camera.image.clamp();
-    camera.image.save(output);
+    auto stringVal = s.erase(0, position + 1);
+    try { value = std::stof(stringVal); }
+    catch (std::out_of_range& e) { throw std::out_of_range(stringVal + " is out of float range"); }
+    catch (std::invalid_argument& e) { throw std::invalid_argument(stringVal + " is not a valid number"); }
+
+    floatVariables[key] = value;
+}
+
+// Render command to generate images from scene files
+void render(const  std::string& input, const std::string& output, int width, float aspectRatio, float a, float gamma, const std::vector<std::string>& floatBuffer,
+            const std::string& algorithm, int AAsamples, int nRays, int maxDepth, int russianRouletteLimit) {
+    std::ifstream file(input);
+    if (file.fail()) { // we already check using CLI11 but you never know
+        std::cout << "ERROR: impossible to open file \"" + input + "\"" << std::endl;
+        exit(-1);
+    }
+
+    std::unordered_map<std::string, float> floatVariables;
+    for (auto s : floatBuffer) {
+        validateFloatVariable(s, floatVariables);
+    }
+
+    Scene scene;
+    InputStream stream(file, input);
+
+    scene.parseScene(stream, floatVariables);
+    if (scene.camera == nullptr) // default camera
+        scene.camera = std::make_shared<Camera>("perspective", 1., 100., 1., translation(-1., 0., 0.));
+
+    // reshape the image from terminal
+    if (aspectRatio > 0.) scene.camera->aspectRatio = aspectRatio;
+    if (width > 0) scene.camera->imageWidth = width;
+    if (width > 0 || aspectRatio > 0.) {
+        scene.camera->imageHeight = scene.camera->imageWidth / scene.camera->aspectRatio;
+        scene.camera->image = HDRImage(scene.camera->imageWidth, scene.camera->imageHeight);
+    }
+
+    if (algorithm == "path")
+        scene.camera->render(Renderers::PathTracer, AAsamples, scene.world, scene.camera->pcg, nRays, maxDepth, russianRouletteLimit);
+    else if (algorithm == "onoff")
+        scene.camera->render(Renderers::OnOff, AAsamples, scene.world);
+    else if (algorithm == "flat")
+        scene.camera->render(Renderers::Flat, AAsamples, scene.world);
+    else if (algorithm == "light")
+        scene.camera->render(Renderers::PointLight, AAsamples, scene.world);
+    else {
+        std::cout << "ERROR: \"" + algorithm + "\" is not a supported rendering algorithm\n" +
+                     "supported algorithms are: \"path\", \"onoff\", \"flat\", \"light\", see --help for more information" << std::endl;
+        exit(-1);
+    }
+
+    scene.camera->image.save(std::filesystem::path(output).stem().string() + ".pfm"); // saves the rendered pfm
+    scene.camera->image.normalize(a);
+    scene.camera->image.clamp();
+    scene.camera->image.save(output, gamma);
 }
 
 
 
 int main(int argc, char* argv[]) {
-    CLI::App app{"RayTracer CLI - convert or demo mode"};
+    CLI::App app{"RayTracer CLI - convert or render"};
 
-    // Convert Command
-    std::string inputFile, outputFile;
+    std::string inputFile = "in.pfm", outputFile = "image.png";
     float a = 1.0f, gamma = 1.0f;
 
+    // Convert Command
     auto convertCommand = app.add_subcommand("convert", "Convert a .pfm file to another format");
-    convertCommand->add_option("input,-i,--input", inputFile, "Input .pfm file")->required();
-    convertCommand->add_option("a,-a,--normalization", a, "Normalization factor, defaults to 1")->required();
-    convertCommand->add_option("gamma,-g,--gamma", gamma, "Gamma correction, defaults to 1")->required();
+    convertCommand->add_option("input,-i,--input", inputFile, "Input .pfm file")->required()->check(CLI::ExistingPath);
+    convertCommand->add_option("a,-a,--normalization", a, "Normalization factor, defaults to 1")->required()->check(CLI::PositiveNumber);
+    convertCommand->add_option("gamma,-g,--gamma", gamma, "Gamma correction, defaults to 1")->required()->check(CLI::PositiveNumber);
     convertCommand->add_option("output,-o,--output", outputFile, "Output image file")->required();
 
-    // Demo Command
-    std::string demoOutput = "demo.png";
-    float angle = 0.0f;
+    // Render Command
     int nRays = 3, maxDepth = 5, russianRouletteLimit = 3, AAsamples = 4;
+    std::string algorithm = "path";
+    int imageWidth = 0;
+    float aspectRatio = 0.;
+    std::vector<std::string> floatBuffer{};
+    std::unordered_map<std::string, float> floatVariables;
 
-    auto demoCommand = app.add_subcommand("demo", "Generate a demo ray-traced image, this always saves a \"demo.pfm\" image");
-    demoCommand->add_option("output,-o,--output", demoOutput, "Output file for the demo .png or .jpeg image");
-    demoCommand->add_option("-A,--AA-samples", AAsamples, "Number of samples per pixel used for anti-aliasing");
-    demoCommand->add_option("--angle", angle, "Angle of the rotation around the Z axis, in degrees");
-    demoCommand->add_option("-n,--ray-number", nRays, "Path tracer only, number of rays sent from every hit point");
-    demoCommand->add_option("-d,--max-depth", maxDepth, "Path tracer only, maximum ray depth");
-    demoCommand->add_option("-r,--rr-limit", russianRouletteLimit, "Path tracer only, ray depth where russian roulette starts");
+    auto renderCommand = app.add_subcommand("render", "Generate a ray-traced image");
+    renderCommand->add_option("input,-i,--input", inputFile, "Input .txt file describing the scene to render")->required()->check(CLI::ExistingPath);
+    renderCommand->add_option("output,-o,--output", outputFile, "Output file for the rendered .png or .jpeg image, a .pfm image is always saved");
+    renderCommand->add_option("-w,--width", imageWidth, "Width of the output image in pixels, overwrites the one defined for the camera")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-r,--aspect-ratio", aspectRatio, "Aspect ratio of the output image, overwrites the one defined for the camera")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-a,--norm", a, "Output image normalization factor, defaults to 1")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-g,--gamma", gamma, "Output image gamma correction, defaults to 1")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-A,--AA-samples", AAsamples, "Number of samples per pixel used for anti-aliasing")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-n,--ray-number", nRays, "Path tracer only, number of rays sent from every hit point")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-d,--max-depth", maxDepth, "Path tracer only, maximum ray depth")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-l,--rr-limit", russianRouletteLimit, "Path tracer only, ray depth where russian roulette starts")->check(CLI::NonNegativeNumber);
+    renderCommand->add_option("-R,--algo", algorithm, "Algorithm to use for rendering: \"path\" (path tracing), \"onoff\", \"flat\", \"light\" (point light tracer)")->check(CLI::IsMember({"path", "onoff", "flat", "light"}));
+    renderCommand->add_option("-f,--float", floatBuffer, "Declare named float variables, overwrites the ones with the same name in the input file. Syntax: name:value");
+
+
 
     CLI11_PARSE(app, argc, argv);
-
-
 
     if (*convertCommand) {
         HDRImage image(inputFile);
@@ -67,8 +127,8 @@ int main(int argc, char* argv[]) {
         image.clamp();
         image.save(outputFile, gamma);
     }
-    else if (*demoCommand) {
-        demo(demoOutput, nRays, maxDepth, russianRouletteLimit, AAsamples, angle);
+    else if (*renderCommand) {
+        render(inputFile, outputFile, imageWidth, aspectRatio, a, gamma, floatBuffer, algorithm, AAsamples, nRays, maxDepth, russianRouletteLimit);
     }
     else {
         std::cout << "Program usage: " << argv[0] << " [demo or convert]\n"
