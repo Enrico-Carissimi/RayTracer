96a1f432cb7eb0875fc22450c5841a2dbed3274a
enrico <enrico.carissimi1@studenti.unimi.it>
2025-07-15 19:55:57 +0200

    add optional blurriness to SpecularMaterial
    and declare some numbers explicitly as floats

--- include/materials.hpp
+++ include/materials.hpp
@@ -25,7 +25,7 @@ public:
  */
 class UniformTexture : public Texture {
 public:
-    UniformTexture() : _color(Color(0., 0., 0.)) {}
+    UniformTexture() : _color(Color()) {}
     UniformTexture(const Color& c) : _color(c) {}
 
     Color color(const Vec2& uv) const override {
@@ -42,7 +42,7 @@ private:
  */
 class CheckeredTexture : public Texture {
 public:
-    CheckeredTexture() : _color1(Color(0., 0., 0.)), _color2(Color(1., 1., 1.)), _nSteps(8) {}
+    CheckeredTexture() : _color1(Color(0.0f, 0.0f, 0.0f)), _color2(Color(1.0f, 1.0f, 1.0f)), _nSteps(8) {}
     CheckeredTexture(const Color& c1, const Color& c2, int nSteps = 8) : _color1(c1), _color2(c2), _nSteps(nSteps) {}
 
     Color color(const Vec2& uv) const override {
@@ -62,7 +62,7 @@ private:
  */
 class ImageTexture : public Texture {
 public:
-    ImageTexture(const std::string& name) : _PFM(name) {_PFM.normalize(1.); _PFM.clamp();}
+    ImageTexture(const std::string& name) : _PFM(name) {_PFM.normalize(1.0f); _PFM.clamp();}
     ImageTexture(const HDRImage& image) : _PFM(image) {}
 
     Color color(const Vec2& uv) const override {
@@ -88,7 +88,7 @@ private:
  */
 class Material {
 public:
-    Material() : _texture(std::make_shared<UniformTexture>(Color(0., 0., 0.))), _emittedRadiance(std::make_shared<UniformTexture>(Color(0., 0., 0.))) {};
+    Material() : _texture(std::make_shared<UniformTexture>(Color())), _emittedRadiance(std::make_shared<UniformTexture>(Color())) {};
     Material(std::shared_ptr<Texture> texture, std::shared_ptr<Texture> emittedRadiance = std::make_shared<UniformTexture>(Color()))
         : _texture(texture), _emittedRadiance(emittedRadiance) {}
     virtual ~Material() = default;
@@ -111,11 +111,11 @@ protected:
  */
 class DiffuseMaterial : public Material {
 public:
-    DiffuseMaterial() : Material(), _reflectance(1.) {}
-    DiffuseMaterial(std::shared_ptr<Texture> texture, std::shared_ptr<Texture> emittedRadiance = std::make_shared<UniformTexture>(Color()), float reflectance = 1.)
+    DiffuseMaterial() : Material(), _reflectance(1.0f) {}
+    DiffuseMaterial(std::shared_ptr<Texture> texture, std::shared_ptr<Texture> emittedRadiance = std::make_shared<UniformTexture>(Color()), float reflectance = 1.0f)
         : Material(texture, emittedRadiance), _reflectance(reflectance / PI) {}
 
-    Color eval(const Vec2& uv, float thetaIn = 0., float thetaOut = 0.) const override {
+    Color eval(const Vec2& uv, float thetaIn = 0.0f, float thetaOut = 0.0f) const override {
         return _texture->color(uv) * _reflectance;
     }
 
@@ -125,10 +125,10 @@ public:
         Vec3 e1, e2;
         createONB(n, e1, e2);
         float cos2 = pcg.random();
-        float cosTheta = std::sqrt(cos2), sinTheta = std::sqrt(1. - cos2);
-        float phi = 2. * PI * pcg.random();
+        float cosTheta = std::sqrt(cos2), sinTheta = std::sqrt(1.0f - cos2);
+        float phi = 2.0f * PI * pcg.random();
 
-        return Ray(interactionPoint, e1 * std::cos(phi) * cosTheta + e2 * std::sin(phi) * cosTheta + n * sinTheta, 1e-5, INF, depth);
+        return Ray(interactionPoint, e1 * std::cos(phi) * cosTheta + e2 * std::sin(phi) * cosTheta + n * sinTheta, 1e-5f, INF, depth);
     }
 
 private:
@@ -137,19 +137,20 @@ private:
 
 /**
  * @class SpecularMaterial
- * @brief Perfect mirror-like reflective material.
+ * @brief Mirror-like reflective material, with adjustable "blurriness".
  */
 class SpecularMaterial : public Material {
 public:
-    SpecularMaterial() : Material(), _thresholdAngleRad(PI / 1800.0) {}
-    SpecularMaterial(std::shared_ptr<Texture> texture, std::shared_ptr<Texture> emittedRadiance = std::make_shared<UniformTexture>(Color()), float thresholdAngleRad = PI / 1800.0)
-        : Material(texture, emittedRadiance), _thresholdAngleRad(thresholdAngleRad) {}
+    SpecularMaterial() : Material(), _blur(0.0f), _thresholdAngleRad(PI / 1800.0f) {}
+    SpecularMaterial(std::shared_ptr<Texture> texture, std::shared_ptr<Texture> emittedRadiance = std::make_shared<UniformTexture>(Color()),
+                     float blur = 0.0f, float thresholdAngleRad = PI / 1800.0f)
+        : Material(texture, emittedRadiance), _blur(blur), _thresholdAngleRad(thresholdAngleRad) {}
 
     Color eval(const Vec2& uv, float thetaIn, float thetaOut) const override {
         if (std::abs(thetaIn - thetaOut) < _thresholdAngleRad) {
             return _texture->color(uv);
         } else {
-            return Color(0.0, 0.0, 0.0);
+            return Color(0.0f, 0.0f, 0.0f);
         }
     };
 
@@ -157,16 +158,23 @@ public:
                    const Normal3& normal, int depth) const override {
         Vec3 rayDir = incomingDir.normalize();
         Vec3 norm(normal.x, normal.y, normal.z);
-        norm = norm.normalize();
-        double dotProd = dot(norm, rayDir);
 
-        Vec3 reflectedDir = rayDir - norm * 2.0 * dotProd;
+        // normalize "norm" here to avoid a call to std::sqrt
+        Vec3 reflectedDir = rayDir - norm * 2.0f * dot(norm, rayDir) / norm.norm2();
 
-        return Ray{interactionPoint, reflectedDir, 1e-5, INF, depth};
+        // Moves the reflected direction by a random vector of length _blur.
+        // I don't think this is physical, but is probably better than sampling
+        // the cone uniformly.
+        // _blur should also be less then 1, otherwise the ray might go inside the shape.
+        if (_blur > 0.0f && _blur < 1.0f) {
+            reflectedDir += _blur * randomVersor(pcg);
+        }
+
+        return Ray{interactionPoint, reflectedDir, 1e-5f, INF, depth};
     }
 
 private:
-    double _thresholdAngleRad;
+    float _blur, _thresholdAngleRad;
 };
 
 #endif
