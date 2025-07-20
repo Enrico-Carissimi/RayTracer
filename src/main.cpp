34693a357b0c82efed09cfa90879a7766ea7912e
enrico <enrico.carissimi1@studenti.unimi.it>
2025-07-20 11:53:46 +0200

    improve CLI parameter decriptions

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,16 @@ void render(const  std::string& input, const std::string& output, int width, flo
 int main(int argc, char* argv[]) {
     CLI::App app{"RayTracer CLI - convert or render"};
 
-    std::string inputFile = "in.pfm", outputFile = "image.png";
+    std::string inputFile, outputFile = "image.png";
     float a = 1.0f, gamma = 1.0f, luminosity = 0.0f;
 
     // Convert Command
-    auto convertCommand = app.add_subcommand("convert", "Convert a .pfm file to another format");
-    convertCommand->add_option("input,-i,--input", inputFile, "Input .pfm file")->required()->check(CLI::ExistingPath);
-    convertCommand->add_option("a,-a,--normalization", a, "Normalization factor")->required()->check(CLI::PositiveNumber);
-    convertCommand->add_option("gamma,-g,--gamma", gamma, "Gamma correction")->required()->check(CLI::PositiveNumber);
-    convertCommand->add_option("output,-o,--output", outputFile, "Output image file")->required();
-    convertCommand->add_option("-l,--luminosity", luminosity, "Manually set the luminosity of the image, useful if it's dark")->check(CLI::NonNegativeNumber);
+    auto convertCommand = app.add_subcommand("convert", "Convert a .pfm file to another format.");
+    convertCommand->add_option("input,-i,--input", inputFile, "Input .pfm file.")->required()->check(CLI::ExistingPath);
+    convertCommand->add_option("a,-a,--normalization", a, "Normalization factor.")->required()->check(CLI::PositiveNumber);
+    convertCommand->add_option("gamma,-g,--gamma", gamma, "Gamma correction factor.")->required()->check(CLI::PositiveNumber);
+    convertCommand->add_option("output,-o,--output", outputFile, "Output image file.")->required();
+    convertCommand->add_option("-l,--luminosity", luminosity, "Manually set the luminosity of the image, useful if it's dark.")->check(CLI::NonNegativeNumber);
 
     // Render Command
     int nRays = 3, maxDepth = 5, russianRouletteLimit = 3, AAsamples = 4;
@@ -33,24 +33,24 @@ int main(int argc, char* argv[]) {
     float aspectRatio = 0.0f;
     std::vector<std::string> floatBuffer{};
     std::unordered_map<std::string, float> floatVariables;
-    uint64_t seed = 42, sequence = 54; 
+    uint64_t seed = 42, sequence = 54;
 
-    auto renderCommand = app.add_subcommand("render", "Generate a ray-traced image");
-    renderCommand->add_option("input,-i,--input", inputFile, "Input .txt file describing the scene to render")->required()->check(CLI::ExistingPath);
-    renderCommand->add_option("output,-o,--output", outputFile, "Output file for the rendered .png or .jpeg image, a .pfm image is always saved");
-    renderCommand->add_option("-w,--width", imageWidth, "Width of the output image in pixels, overwrites the one defined for the camera")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-a,--norm", a, "Output image normalization factor, defaults to 1")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-r,--aspect-ratio", aspectRatio, "Aspect ratio of the output image, overwrites the one defined for the camera")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-l,--luminosity", luminosity, "Manually set the luminosity of the image, useful if it's dark")->check(CLI::NonNegativeNumber);
-    renderCommand->add_option("-g,--gamma", gamma, "Output image gamma correction, defaults to 1")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-A,--AA-samples", AAsamples, "Number of samples per pixel used for anti-aliasing, defaults to 4")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-n,--ray-number", nRays, "Path tracer only, number of rays sent from every hit point, defaults to 3")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-d,--max-depth", maxDepth, "Path tracer only, maximum ray depth, defaults to 5")->check(CLI::PositiveNumber);
-    renderCommand->add_option("-L,--rr-limit", russianRouletteLimit, "Path tracer only, ray depth where russian roulette starts. If is bigger the max-depth, russian roulette will never start. Defaults to 3")->check(CLI::NonNegativeNumber);
-    renderCommand->add_option("-R,--algo", algorithm, "Algorithm to use for rendering: \"path\" (path tracing, default), \"onoff\", \"flat\", \"light\" (point light tracer)")->check(CLI::IsMember({"path", "onoff", "flat", "light"}));
-    renderCommand->add_option("-f,--float", floatBuffer, "Declare named float variables, overwrites the ones with the same name in the input file. Syntax: name:value");
-    renderCommand->add_option("--seed", seed, "Seed of the random number generator, defaults to 42")->check(CLI::NonNegativeNumber);
-    renderCommand->add_option("--sequence", sequence, "Sequence identifier of the random number generator, defaults to 54")->check(CLI::NonNegativeNumber);
+    auto renderCommand = app.add_subcommand("render", "Generate a ray-traced image.");
+    renderCommand->add_option("input,-i,--input", inputFile, "Input .txt file describing the scene to render.")->required()->check(CLI::ExistingPath);
+    renderCommand->add_option("output,-o,--output", outputFile, "Output file for the rendered .png or .jpeg image, a .pfm image with the same file name is always saved.");
+    renderCommand->add_option("-w,--width", imageWidth, "Width of the output image in pixels, overwrites the one defined for the camera.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-r,--aspect-ratio", aspectRatio, "Aspect ratio of the output image, overwrites the one defined for the camera.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-a,--norm", a, "Output image normalization factor, defaults to 1.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-g,--gamma", gamma, "Output image gamma correction, defaults to 1.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-l,--luminosity", luminosity, "Manually set the luminosity of the image, useful if it's dark.")->check(CLI::NonNegativeNumber);
+    renderCommand->add_option("-A,--AA-samples", AAsamples, "Number of samples per pixel used for anti-aliasing, defaults to 4. If the number is a perfect square, uses stratified sampling.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-n,--ray-number", nRays, "Path tracer only, number of rays sent from every hit point, defaults to 3.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-d,--max-depth", maxDepth, "Path tracer only, maximum ray depth, defaults to 5.")->check(CLI::PositiveNumber);
+    renderCommand->add_option("-L,--rr-limit", russianRouletteLimit, "Path tracer only, ray depth where russian roulette starts. If it's bigger the max-depth, russian roulette will never start. Defaults to 3.")->check(CLI::NonNegativeNumber);
+    renderCommand->add_option("-R,--algo", algorithm, "Algorithm to use for rendering: \"path\" (path tracing, default), \"onoff\", \"flat\", \"light\" (point light tracer).")->check(CLI::IsMember({"path", "onoff", "flat", "light"}));
+    renderCommand->add_option("-f,--float", floatBuffer, "Declare named float variables, overwrites the ones with the same name in the input file. Syntax: name:value.");
+    renderCommand->add_option("--seed", seed, "Seed of the random number generator, defaults to 42.")->check(CLI::NonNegativeNumber);
+    renderCommand->add_option("--sequence", sequence, "Sequence identifier of the random number generator, defaults to 54.")->check(CLI::NonNegativeNumber);
 
 
 
